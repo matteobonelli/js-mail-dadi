c8f7e0955ce2bf4b4470a20a5438c268cb459b16
matteobonelli <matteobonelli08@gmail.com>
2023-10-12 15:04:37 +0200

    event listener for button to take input value created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-let int;
+const button = document.querySelector('.btn-light');
+
+button.addEventListener('click', function(){
+    const emailUser = document.querySelector('input').value;
+    console.log(emailUser);
+})
